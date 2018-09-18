5c1a5bb933e3a2b60d5f253dedd975e2464f1a05
Ian Zaragoza <ianzaragoza@msn.com>
2018-09-18 20:15:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 
-function deleteFromObjectByKey(object, key) {
+function deleteFromObjectByKey(newObject, key) {
   delete newObject[key]
   return newObject
 }

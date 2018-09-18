5d9447882da2aa7732b48d8a05a86a2e24014239
Ian Zaragoza <ianzaragoza@msn.com>
2018-09-18 19:49:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,5 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 
 function deleteFromObjectByKey(object, key) {
   delete newObject[key]
-  return object
+  return newObject
 }

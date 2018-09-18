f94afb1f98b9cab5ae0e65be4b1306425259c78a
Ian Zaragoza <ianzaragoza@msn.com>
2018-09-18 20:12:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
   var recipes = {
     object: 'recipes'
   }
-
+  var newObject = Object.assign({}, object)
+  }
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({object}, {[key]: value})
 }
